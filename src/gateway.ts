ae4d0159ee3907d0d81976cea51b683ad424df89
Adebayo Mustafa <adebsalert@gmail.com>
2021-12-31 17:31:55 +0100

    added rate limit feature to all route

--- src/gateway.ts
+++ src/gateway.ts
@@ -3,13 +3,21 @@ import helmet from 'helmet'
 const app = express()
 import * as registry from './registry/registry.json'
 import { router } from './routes'
-import { PORT } from './util/config'
+import { PORT, RATE_LIMIT_PER_HOUR } from './util/config'
+import rateLimit from 'express-rate-limit'
 
 // init the registry
 // const registryData: {[index: string]:any} = registry;
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 60 minutes
+	max: parseInt(RATE_LIMIT_PER_HOUR), // Limit each IP to 100 requests per `window` (here, per 60 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 app.use(express.json())
 app.use(helmet())
 
--- src/util/config.ts
+++ src/util/config.ts
@@ -14,3 +14,4 @@ function throwIfUndefined<T>(secret: T | undefined, name?: string): T {
 export const PORT = throwIfUndefined(process.env.PORT, 'PORT');
 export const APP = throwIfUndefined(process.env.APP, 'APP');
 export const NODE_ENV = throwIfUndefined(process.env.NODE_ENV, 'NODE_ENV');
+export const RATE_LIMIT_PER_HOUR = throwIfUndefined(process.env.RATE_LIMIT_PER_HOUR, 'RATE_LIMIT_PER_HOUR');
